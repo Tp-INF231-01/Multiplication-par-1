a3330561fdfa2130a96b58d19f73b784f0227558
nkengnemiguel80-lab <nkengnemiguel80@gmail.com>
2025-09-24 17:56:49 +0100

    Add files via upload

---
+++ multiplication+1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int a,b,mul;
+printf("entre vos nombre a multiply");
+scanf("%d %d",&a,&b);
+for(int i=0; i<b; i++){
+for(int j=0; j<a; j++){
+mul+=1;
+
+}
+
+
+}
+printf("%d",mul);
+return 0;
+
+
+
+}
